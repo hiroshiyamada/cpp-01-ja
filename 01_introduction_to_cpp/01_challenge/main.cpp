fac30ac904ed26f72f09fd421680035203e3200d
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2023-12-11 15:26:44 +0900

    Add m1-u1-c1 codes

---
+++ 01_introduction_to_cpp/01_challenge/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// 不足している値、データ型、その他のコードを追加してプログラムを完成させてください。
+int main() {
+    int myNumber = 10;
+
+    int anotherNumber = 5;
+    
+    int answer = myNumber + anotherNumber;
+
+    std::cout << answer << std::endl;
+    return 0;
+}
